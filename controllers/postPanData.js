332e00c4e5040ff548c93b44961176b967449289
Kushagra Sharma <kushasharma@paypal.com>
2017-10-28 20:39:07 +0530

    some refactoring

--- controllers/postPanData.js
+++ controllers/postPanData.js
@@ -23,34 +23,47 @@ const pushToFirebase = function pushToFirebase(firebasePayload) {
 }
 
 const extractImgBufferData = function(req) {
+  console.log(req.files);
   const fileName = Object.keys(req.files)[0];
-  const imgDataBuffer = new Buffer(req.files.sample.data);
+  const imgDataBuffer = new Buffer(req.files[fileName].data);
   const base64EncodedImgData = imgDataBuffer.toString('base64');
   return base64EncodedImgData;
 }
 
-const processImage = function(req, res) {
-  if (!req.files){
+const validatePanImage = function(image) {
+  vision.labelDetection(image).then(response => {
+    
+  }).catch(err => {
+    console.error(err);
+    res.status(500);
+    return res.send("Error in labelDetection");
+  });
+}
+
+const processPanImage = function(req, res) {
+  if (!req.files || (Object.keys(req.files).length === 0 && req.files.constructor === Object)){
+    console.error("req.files is not populated");
     res.status(500)
-    return res.send("Something went wrong");
+    return res.send("Error in processPanImage");
   } 
   const imgBufferData = extractImgBufferData(req);
   const image = {
     content: imgBufferData
   }
+  res.send("OK")
 
-  vision.textDetection(image).then(response => {
-    const parsedResponse = helper.parsePanCardResponse(response);
-    const firebasePayload = prepareFirebasePayload(parsedResponse, imgBufferData)
-    pushToFirebase(firebasePayload);
-    res.json(parsedResponse);
-  }).catch(err => {
-    console.error(err);
-    res.status(500);
-    return res.send("Something went wrong");
-  });
+  // vision.textDetection(image).then(response => {
+  //   const parsedResponse = helper.parsePanCardResponse(response);
+  //   const firebasePayload = prepareFirebasePayload(parsedResponse, imgBufferData)
+  //   pushToFirebase(firebasePayload);
+  //   res.json(parsedResponse);
+  // }).catch(err => {
+  //   console.error(err);
+  //   res.status(500);
+  //   return res.send("Error in textDetection");
+  // });
 }
 
 module.exports = {
-  processImage: processImage
+  processImage: processPanImage
 }
