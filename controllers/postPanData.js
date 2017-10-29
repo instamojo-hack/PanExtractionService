551a5279c1e113ee2fe8dcc11967b50a074dbf1a
Kushagra Sharma <kushasharma@paypal.com>
2017-10-29 10:39:39 +0530

    add image sharpening

--- controllers/postPanData.js
+++ controllers/postPanData.js
@@ -8,6 +8,7 @@ const admin = require("../services/firebaseAdmin");
 const googleVisionHelper = require("../lib/googleVisionHelper");
 const firebaseHelper = require("../lib/fireBaseHelper");
 const path = require("path");
+const sharp = require("sharp");
 
 const extractImgBufferData = function(req) {
   console.log(req.files);
@@ -15,6 +16,22 @@ const extractImgBufferData = function(req) {
   const imgDataBuffer = new Buffer(req.files[fileName].data);
   const base64EncodedImgData = imgDataBuffer.toString('base64');
   return base64EncodedImgData;
+}
+
+const sharpenImage = function(imgBufferData, cb) {
+  const SHARPEN_SIGMA =2.5
+  var buf = Buffer.from(imgBufferData, 'base64');
+  //sharp(buf).toFile('./.temp/original.jpg', (err, info) => console.error(err));
+  const processedImage = sharp(buf).sharpen(SHARPEN_SIGMA);
+  //processedImage.toFile('./.temp/processed.jpg', (err, info) => console.error(err));
+  processedImage.toBuffer((err, data, info) => {
+    if (err){
+      cb(imgBufferData);
+    }
+    //console.log(`Buffer: ${JSON.stringify(data)}`);
+    cb(Buffer.from(data, 'base64'));
+  });
+  return;
 }
 
 const processPanImage = function(req, res) {
@@ -30,62 +47,64 @@ const processPanImage = function(req, res) {
     }
     imgBufferData = extractImgBufferData(req);
   }
-  const image = {
-    content: imgBufferData
-  }
-  async.parallel({
-    validateLabels: function validateLabels(cb) {
-      console.log("Running label detection");
-      vision.labelDetection(image).then(response => {
-        console.log(JSON.stringify(response));
-        const IdentityDocumentConfidenceScore = googleVisionHelper.parseLabelDetectionResponse(response);
-        const PASS_THRESHOLD_CONFIDENCE = 0.75;
-        const passed = IdentityDocumentConfidenceScore > PASS_THRESHOLD_CONFIDENCE;
-        console.log(`IdentityDocumentConfidenceScore: ${IdentityDocumentConfidenceScore} ; passed: ${passed}`);
-        cb(null, passed);
-      }).catch(err => {
-        cb(err, null);
-      });
+  sharpenImage(imgBufferData, function(processedBase64Data) {
+    const image = {
+      content: processedBase64Data
+    }
+    async.parallel({
+      validateLabels: function validateLabels(cb) {
+        console.log("Running label detection");
+        vision.labelDetection(image).then(response => {
+          console.log(JSON.stringify(response));
+          const IdentityDocumentConfidenceScore = googleVisionHelper.parseLabelDetectionResponse(response);
+          const PASS_THRESHOLD_CONFIDENCE = 0.75;
+          const passed = IdentityDocumentConfidenceScore > PASS_THRESHOLD_CONFIDENCE;
+          console.log(`IdentityDocumentConfidenceScore: ${IdentityDocumentConfidenceScore} ; passed: ${passed}`);
+          cb(null, passed);
+        }).catch(err => {
+          cb(err, null);
+        });
+      },
+      validateWeb: function validateLabels(cb) {
+        vision.webDetection(image).then(response => {
+          console.log(JSON.stringify(response));
+          const panConfidenceScore = googleVisionHelper.parseWebDetectionResponse(response);
+          const PASS_THRESHOLD_CONFIDENCE = -1;
+          const passed = panConfidenceScore > PASS_THRESHOLD_CONFIDENCE;
+          console.log(`panConfidenceScore: ${panConfidenceScore} ; passed: ${passed}`);
+          cb(null, passed);
+        }).catch(err => {
+          cb(err, null);
+        });
+      }
     },
-    validateWeb: function validateLabels(cb) {
-      vision.webDetection(image).then(response => {
-        console.log(JSON.stringify(response));
-        const panConfidenceScore = googleVisionHelper.parseWebDetectionResponse(response);
-        const PASS_THRESHOLD_CONFIDENCE = 0.40;
-        const passed = panConfidenceScore > PASS_THRESHOLD_CONFIDENCE;
-        console.log(`panConfidenceScore: ${panConfidenceScore} ; passed: ${passed}`);
-        cb(null, passed);
-      }).catch(err => {
-        cb(err, null);
-      });
-    }
-  },
-  function (err, results) {
-    if(err) {
-      console.error(err);
-      res.status(500);
-      return res.sendFile(path.resolve("public/template/500.html"));
-    }
-    if (!(results.validateLabels && results.validateWeb)) {
-      console.error("PAN validation failed.");
-      res.status(422);
-      return res.sendFile(path.resolve("public/template/422.html"));
-    }
-    console.log("All validations passed!");
-  
-    vision.textDetection(image).then(response => {
-      const parsedResponse = googleVisionHelper.parseTextDetectionResponse(response);
-      if (!parsedResponse) {
+    function (err, results) {
+      if(err) {
+        console.error(err);
+        res.status(500);
+        return res.sendFile(path.resolve("public/template/500.html"));
+      }
+      if (!(results.validateLabels && results.validateWeb)) {
+        console.error("PAN validation failed.");
         res.status(422);
         return res.sendFile(path.resolve("public/template/422.html"));
       }
-      const firebasePayload = firebaseHelper.prepareFirebasePayload(parsedResponse, imgBufferData)
-      firebaseHelper.pushToFirebase(firebasePayload);
-      return res.json(parsedResponse);
-    }).catch(err => {
-      console.error(err);
-      res.status(500);
-      return res.send("Error in textDetection");
+      console.log("All validations passed!");
+    
+      vision.textDetection(image).then(response => {
+        const parsedResponse = googleVisionHelper.parseTextDetectionResponse(response);
+        if (!parsedResponse) {
+          res.status(422);
+          return res.sendFile(path.resolve("public/template/422.html"));
+        }
+        const firebasePayload = firebaseHelper.prepareFirebasePayload(parsedResponse, imgBufferData)
+        firebaseHelper.pushToFirebase(firebasePayload);
+        return res.json(parsedResponse);
+      }).catch(err => {
+        console.error(err);
+        res.status(500);
+        return res.send("Error in textDetection");
+      });
     });
   });
 }
